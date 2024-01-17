c48fe243e98079c2f44e1266c6b87f43a0904ed5
DenisTsirlin <denis.tsirlin@gmail.com>
2024-01-17 15:07:54 +0200

    ee

--- proj1_calc/src/Class Comps/CCCalc.jsx
+++ proj1_calc/src/Class Comps/CCCalc.jsx
@@ -7,59 +7,60 @@ export default class CCMath extends Component {
             num1: 0,
             num2: 0,
             result: 0,
-           
+
         };
     }
+    
     chgNum1 = (e) => {
         this.setState({ num1: Number(e.target.value) });
-        };
+    };
     chgNum2 = (e) => {
         this.setState({ num2: Number(e.target.value) });
-        };
-     handleAddition = () => {
+    };
+    handleAddition = () => {
         const { num1, num2 } = this.state;
         console.log("num1:", num1, "num2:", num2);
         this.setState({ result: num1 + num2 });
-        };
-       
-     handleMinus= () => { 
+    };
+
+    handleMinus = () => {
         const { num1, num2 } = this.state;
         console.log("num1:", num1, "num2:", num2);
-        this.setState({ result: num1 - num2});
+        this.setState({ result: num1 - num2 });
 
-        };
-        
-     handleMultiplication= () => { 
+    };
+
+    handleMultiplication = () => {
         const { num1, num2 } = this.state;
         console.log("num1:", num1, "num2:", num2);
-        this.setState({ result: num1 * num2});
+        this.setState({ result: num1 * num2 });
 
-        };
-     handleDivision= () => { 
+    };
+    handleDivision = () => {
         const { num1, num2 } = this.state;
         console.log("num1:", num1, "num2:", num2);
-        this.setState({ result: num1 / num2});
+        this.setState({ result: num1 / num2 });
 
-     };
-    
-     
+    };
+
+
     render() {
-        const { result} = this.state
+        const { result } = this.state
 
         return (
             <div>
-                Num1: <input type="number" onChange={this.chgNum1}/><br />
-                Num2: <input type="number" onChange={this.chgNum2}/> <br/> <br />
+                Num1: <input type="number" onChange={this.chgNum1} /><br />
+                Num2: <input type="number" onChange={this.chgNum2} /> <br /> <br />
                 <div className="my-button">
                     <button onClick={this.handleAddition}>+</button>
-                    <button id="buttonMinus" onClick={this.handleMinus}>-</button><br /> 
+                    <button id="buttonMinus" onClick={this.handleMinus}>-</button><br />
                     <button id="buttonMultiplication" onClick={this.handleMultiplication}>*</button>
                     <button id="buttonDivision" onClick={this.handleDivision}>/</button>
-                </div><br/><br />
-                Result: <input type="number" value={result} readOnly/>
+                </div><br /><br />
+                Result: <input type="number" value={result} readOnly />
                 <br />
             </div>
-            
+
         )
 
     }
